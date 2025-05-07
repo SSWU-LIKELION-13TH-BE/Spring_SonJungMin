b4fbd145f49d67162a6ce7af336d94407ca7cef9
Son <suruna1026@gmail.com>
2025-05-07 20:29:14 +0900

    feat : substract,divide

---
+++ src/main/java/com/likelion/session/dto/Calculator/request/CalculatorDivideRequest.java
@@ -0,0 +1,25 @@
+package com.likelion.session.dto.Calculator.request;
+
+public class CalculatorDivideRequest {
+    private final int number1;
+    private final int number2;
+
+    public CalculatorDivideRequest(int number1, int number2){
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public CalculatorDivideRequest(){
+        this.number1=0;
+        this.number2=0;
+    }
+
+    public int getNumber1(){
+        return number1;
+    }
+
+    public int getNumber2(){
+        return number2;
+    }
+
+}
