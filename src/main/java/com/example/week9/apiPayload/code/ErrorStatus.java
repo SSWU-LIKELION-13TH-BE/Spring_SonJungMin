6a33c574b75e7e62dfe674494a67d9296ddb1b12
Son <suruna1026@gmail.com>
2025-06-04 07:40:53 +0900

    feat : ID_Duplicate_Check

--- src/main/java/com/example/week9/apiPayload/code/ErrorStatus.java
+++ src/main/java/com/example/week9/apiPayload/code/ErrorStatus.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
-    _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."), TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "테스트용 예외입니다.");
+    _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."), TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "테스트용 예외입니다."), USERNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "EXISTSUSER400", "이미 존재하는 사용자입니다.");
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
